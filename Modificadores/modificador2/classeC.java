bd418bb0acd101cdcbef29c7d4f217787af28dc6
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-09-02 17:45:18 -0300

    Add files via upload

---
+++ Modificadores/modificador2/classeC.java
@@ -0,0 +1,14 @@
+package modificador2;
+
+import modificador1.classeA;
+
+public class classeC extends classeA {
+    public static void main(String[] args) {
+        // classeC c = new classeC();
+
+        // System.out.println(c.senha);
+        classeC classeC = new classeC();
+
+        classeC.falar();
+   } 
+}
