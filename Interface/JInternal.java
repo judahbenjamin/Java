707e5c9b3384e52d789e663be46e0ba77e47cf20
Judah Benjamin <160127048+judahbenjamin@users.noreply.github.com>
2024-09-19 16:22:13 -0300

    Update JInternal.java

--- Interface/JInternal.java
+++ Interface/JInternal.java
@@ -5,35 +5,40 @@ package atividade13;
  * Por Judah Benjamin, 19/09/2024
  */
 import javax.swing.*;
-import sun.audio.*;
-import com.sun.java.util.*;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.*;
 import java.awt.Event.*;
 import java.awt.event.*;
 import java.io.*;
+import javax.swing.*;
 
 public class JInternal {
-
     public static void main(String[] args) {
         JFrame tela = new JFrame("Tela de teste");
         JDesktopPane deska = new JDesktopPane();
         final JInternalFrame telinha1 = new JInternalFrame("Tela Interna 1", true, true, true, true);
         final JInternalFrame telinha2 = new JInternalFrame("Tela Interna 2", true, true, true, true);
         final JInternalFrame telinha3 = new JInternalFrame("Tela Interna 3", true, true, true, true);
+        final JInternalFrame telinha4 = new JInternalFrame("Tela Interna 3", true, true, true, true);
 
         telinha1.setBounds(0, 0, 400, 500);
         telinha2.setBounds(0, 0, 400, 500);
         telinha3.setBounds(0, 0, 400, 500);
+        telinha4.setBounds(0, 0, 400, 500);
         telinha1.hide();
         telinha2.hide();
         telinha3.hide();
+        telinha4.hide();
         telinha1.setDefaultCloseOperation(JInternalFrame.HIDE_ON_CLOSE);
         telinha2.setDefaultCloseOperation(JInternalFrame.HIDE_ON_CLOSE);
         telinha3.setDefaultCloseOperation(JInternalFrame.HIDE_ON_CLOSE);
+        telinha4.setDefaultCloseOperation(JInternalFrame.HIDE_ON_CLOSE);
 
         JMenuItem t1 = new JMenuItem("Tela Calculadora");
         JMenuItem t2 = new JMenuItem("Exemplo Menu");
         JMenuItem t3 = new JMenuItem("Tela Tabela");
+        JMenuItem t4 = new JMenuItem("Formulário");
         JMenuBar barra = new JMenuBar();
         JMenu calculadora = new JMenu("Calculadora");
         barra.add(calculadora);
@@ -41,9 +46,12 @@ public class JInternal {
         JMenu exemplo = new JMenu("Exemplo Menu");
         barra.add(exemplo);
         exemplo.add(t2);
-        JMenu tabela = new JMenu("Tela Tabela");
-        barra.add(tabela);
-        tabela.add(t3);
+        JMenu t_tabela = new JMenu("Tela Tabela");
+        barra.add(t_tabela);
+        t_tabela.add(t3);
+        JMenu form = new JMenu("Formulário Cadastro");
+        barra.add(form);
+        form.add(t4);
 
         t1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -192,15 +200,14 @@ public class JInternal {
         });
 
         t3.addActionListener(new ActionListener() {
-            public void actionPerfomed(ActionEvent e) {
-                JFrame telinha3 = new JFrame("Tabelas");
-                telinha3.setBounds(0, 0, 1024, 768);
+            public void actionPerformed(ActionEvent e) {
+                JFrame tela = new JFrame("Tabelas");
+                tela.setBounds(0, 0, 1024, 768);
                 JTable tabela = new JTable();
                 tabela.setBounds(10, 10, 200, 400);
-                telinha3.add(tabela);
+                tela.add(tabela);
 
-                String[] colunas = {"Nome", "Salário"};
-
+                String[] colunas = { "Nome", "Salário" };
                 DefaultTableModel modelo = (DefaultTableModel) (new DefaultTableModel() {
                     public boolean isCellEditable(int row, int column) {
                         return false;
@@ -221,25 +228,148 @@ public class JInternal {
                 objetos[0] = "Lula-lá";
                 objetos[1] = "12000";
                 modelo.addRow(objetos);
+                objetos[0] = "Fulano de tal";
+                objetos[1] = "9000";
+                modelo.addRow(objetos);
+                objetos[0] = "Ciclano";
+                objetos[1] = "10900";
+                modelo.addRow(objetos);
 
                 tabela.setModel(modelo);
-                tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 tela.setLayout(null);
                 tela.setVisible(true);
-                
-                tabela.addMouseListener(new MouseListener() {
-                    public void mouseClicked(MouseEvent e) {
-                        if (e.getClickCount() == 2) {
-                            int linha = tabela.getSelectedRow();
-                            String mensagem = (tabela.getValueAt(linha, 0).toString()) + " ganha R$ "
-                                    + (tabela.getValueAt(linha, 1).toString());
-                
+            }
+        });
+
+        t4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFrame form = new JFrame();
+                form.setBounds(300, 300, 630, 1000);
+                form.getContentPane().setBackground(Color.WHITE);
+                form.setLayout(null);
+
+                JLabel titulo = new JLabel();
+                titulo.setText("Cadastro de funcionários");
+                titulo.setBounds(70, 0, 500, 100);
+                titulo.setFont(new Font("Arial", Font.BOLD, 38));
+                titulo.setForeground(Color.black);
+                form.add(titulo);
+
+                JLabel matricula = new JLabel();
+                JTextField campo_matricula = new JTextField();
+                matricula.setText("Matrícula:");
+                matricula.setBounds(70, 70, 500, 100);
+                matricula.setFont(new Font("Arial", Font.ITALIC, 17));
+                campo_matricula.setBounds(70, 150, 468, 40);
+                form.add(matricula);
+                form.add(campo_matricula);
+
+                JLabel nome = new JLabel();
+                JTextField campo_nome = new JTextField();
+                nome.setText("Nome Completo:");
+                nome.setBounds(70, 170, 500, 100);
+                nome.setFont(new Font("Arial", Font.ITALIC, 17));
+                campo_nome.setBounds(70, 250, 468, 40);
+                form.add(nome);
+                form.add(campo_nome);
+
+                JLabel data = new JLabel();
+                JTextField campo_data = new JTextField();
+                data.setText("Data de Nascimento:");
+                data.setBounds(70, 270, 500, 100);
+                data.setFont(new Font("Arial", Font.ITALIC, 17));
+                campo_data.setBounds(70, 350, 468, 40);
+                form.add(data);
+                form.add(campo_data);
+
+                JLabel rg = new JLabel();
+                JTextField campo_rg = new JTextField();
+                rg.setText("RG:");
+                rg.setBounds(70, 370, 500, 100);
+                rg.setFont(new Font("Arial", Font.ITALIC, 17));
+                campo_rg.setBounds(70, 450, 220, 40);
+                form.add(rg);
+                form.add(campo_rg);
+
+                JLabel cpf = new JLabel();
+                JTextField campo_cpf = new JTextField();
+                cpf.setText("CPF:");
+                cpf.setBounds(320, 370, 500, 100);
+                cpf.setFont(new Font("Arial", Font.ITALIC, 17));
+                campo_cpf.setBounds(320, 450, 220, 40);
+                form.add(cpf);
+                form.add(campo_cpf);
+
+                JLabel endereco = new JLabel();
+                JTextField campo_endereco = new JTextField();
+                endereco.setText("Endereço:");
+                endereco.setBounds(70, 470, 500, 100);
+                endereco.setFont(new Font("Arial", Font.ITALIC, 17));
+                campo_endereco.setBounds(70, 550, 220, 40);
+                form.add(endereco);
+                form.add(campo_endereco);
+
+                JLabel cidade = new JLabel();
+                JTextField campo_cidade = new JTextField();
+                cidade.setText("Cidade:");
+                cidade.setBounds(320, 470, 500, 100);
+                cidade.setFont(new Font("Arial", Font.ITALIC, 17));
+                campo_cidade.setBounds(320, 550, 150, 40);
+                form.add(cidade);
+                form.add(campo_cidade);
+
+                JLabel estado = new JLabel();
+                JTextField campo_estado = new JTextField();
+                estado.setText("Estado:");
+                estado.setBounds(485, 470, 500, 100);
+                estado.setFont(new Font("Arial", Font.ITALIC, 17));
+                campo_estado.setBounds(485, 550, 55, 40);
+                form.add(estado);
+                form.add(campo_estado);
+
+                JLabel cep = new JLabel();
+                JTextField campo_cep = new JTextField();
+                cep.setText("CEP:");
+                cep.setBounds(70, 570, 500, 100);
+                cep.setFont(new Font("Arial", Font.ITALIC, 17));
+                campo_cep.setBounds(70, 650, 220, 40);
+                form.add(cep);
+                form.add(campo_cep);
+
+                JLabel setor = new JLabel();
+                JTextField campo_setor = new JTextField();
+                setor.setText("Setor de trabalho:");
+                setor.setBounds(320, 570, 500, 100);
+                setor.setFont(new Font("Arial", Font.ITALIC, 17));
+                campo_setor.setBounds(320, 650, 220, 40);
+                form.add(setor);
+                form.add(campo_setor);
+
+                JLabel cargo = new JLabel();
+                JTextField campo_cargo = new JTextField();
+                cargo.setText("Cargo:");
+                cargo.setBounds(70, 670, 500, 100);
+                cargo.setFont(new Font("Arial", Font.ITALIC, 17));
+                campo_cargo.setBounds(70, 750, 468, 40);
+                form.add(cargo);
+                form.add(campo_cargo);
+
+                JButton botao = new JButton();
+                botao.setText("Enviar");
+                botao.setFont(new Font("Arial", Font.BOLD, 20));
+                botao.setBounds(240, 840, 140, 50);
+                botao.setBackground(Color.BLUE);
+                botao.setForeground(Color.WHITE);
+                form.add(botao);
+
+                form.setVisible(true);
             }
         });
 
         deska.add(telinha1);
         deska.add(telinha2);
         deska.add(telinha3);
+        deska.add(telinha4);
         tela.add(deska);
         tela.setJMenuBar(barra);
 
